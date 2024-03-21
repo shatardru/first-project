a9685271e9a1afcadd343ccfc2d6455014750281
devnuckles <mohammadroman925@gmail.com>
2024-03-21 15:21:45 +0600

    Added test.js file of git commands

---
+++ test.js
@@ -0,0 +1,15 @@
+// Git Commands
+
+/*
+git init 
+git add .
+git commit -m "Commit what changes"
+
+git pull --rebase origin main (Main branch theke code pull kore rebase korlam)
+
+git push --force-with-lease origin branch_name
+
+git branch branch_name (Create a branch )
+git branch (See all branches)
+git switch branch_name (Switch to another branch)
+*/
